b4a0a2fcb574b112e9703d94039e8fa3a6f53fef
kishan kumar <56019933+kishan132@users.noreply.github.com>
2020-04-14 19:16:33 +0530

    add parameter

---
+++ parameter.v
@@ -0,0 +1,18 @@
+//**************************************************************************
+//************** DEFINITION FILES ******************************************
+//**************************************************************************
+
+`define INPUT_FILE "MRImedical.hex"   // INPUT HEX file name
+`define OUTPUT_FILE "outputMRI.bmp"   // OUTPUT BMP file name
+
+//---------------------------------------------------------------------------
+//----------------  LIST OF DIFFERENT OPERATIONS ----------------------------
+//---------------------------------------------------------------------------
+
+`define ORIGINAL_IMAGE
+
+//`define INVERT_OPERATION
+
+//`define BRIGHTNESS_OPERATION
+
+//`define THRESHOLD_OPERATION
